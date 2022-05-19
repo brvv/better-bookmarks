c127117e92eb864fb1ba57fe1a352b13ba341dd0
brvv <vladislav.brovikov@gmail.com>
2022-05-18 20:03:30 -0700

    added background script

--- public/popup/background.js
+++ public/popup/background.js
@@ -1,10 +1,55 @@
-function handleCreated(id, bookmarkInfo) {
-  console.log(`New bookmark ID: ${id}`);
-  console.log(`New bookmark URL: ${bookmarkInfo.url}`);
-}
-console.log("Background script loaded");
-browser.bookmarks.getTree().then((tree) => {
-  console.log(tree);
+const TOOLBAR_ID = "toolbar_____";
+const MENU_ID = "menu________";
+
+const getAllBookmarks = (bookmarkTree, currentPath = "/") => {
+  let bookmarks = [];
+  bookmarkTree.children.forEach((child) => {
+    if (child.type === "folder") {
+      bookmarks = bookmarks.concat(
+        getAllBookmarks(child, currentPath + `${child.title}/`)
+      );
+    } else {
+      bookmarks.push(child);
+    }
+  });
+  return bookmarks;
+};
+
+const printBookmarks = (bookmarks) => {
+  bookmarks.forEach((bookmark) => {
+    const date = new Date(bookmark.dateAdded);
+    console.log(`${bookmark.title} added on ${date.getDate()}`);
+  });
+};
+
+const createRootFolder = async () => {
+  return await browser.bookmarks.create({
+    title: "Better Bookmarks Root",
+    parentId: MENU_ID,
+    type: "folder",
+  });
+};
+
+//Create a root folder for the extension
+
+const setupExtension = async (event) => {
+  createRootFolder().then((node) => {
+    console.log("Create root folder for bookmarks!");
+  });
+};
+
+setupExtension().then(() => {
+  console.log("Successful setup!");
 });
 
-browser.bookmarks.onCreated.addListener(handleCreated);
+/*
+console.log("hello?");
+
+browser.bookmarks.getTree().then((tree) => {
+  const root = tree[0];
+  console.log("here");
+  console.log(root);
+  const bookmarks = getAllBookmarks(root);
+  printBookmarks(bookmarks);
+});
+*/
