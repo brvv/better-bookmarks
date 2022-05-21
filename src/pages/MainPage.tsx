d267a4d1e9c3a9fde0f60f4fcb48a6029b08f543
brvv <vladislav.brovikov@gmail.com>
2022-05-20 19:08:37 -0700

    removed props from page

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -3,27 +3,19 @@ import { BookmarkContainer, CardContainer } from "../components";
 import { getRootId } from "../api";
 import { useParams } from "react-router-dom";
 
-type Props = {
-  parentId?: string;
-};
-
-export const MainPage: React.FC<Props> = ({ parentId = "" }) => {
+export const MainPage: React.FC = () => {
   const params = useParams();
   const [rootId, setRootId] = useState("");
 
   useEffect(() => {
-    console.log("Params are:");
-    console.log(params);
-    if (parentId) {
-      setRootId(parentId);
-    } else if (params.folderId) {
+    if (params.folderId) {
       setRootId(params.folderId);
     } else {
       getRootId().then((id) => {
         setRootId(id);
       });
     }
-  }, [parentId, params]);
+  }, [params]);
 
   return (
     <div className="App">
